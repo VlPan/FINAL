3b588f4d36ccfbbb7fe0fc2a7b573479be527266
VlPan <vladimirpant.bel@gmail.com>
2017-11-29 23:26:49 +0300

    + Advanced Search. Todo: Add popularity and vote Search.

---
+++ src/components/FormControls/selector.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Selector = (props) => {
+    return (
+    <input
+        type="checkbox"
+        name={props.name}
+        value={props.value}
+        readOnly={props.readOnly}
+        checked={props.checked}
+        onChange={props.onChangeHandler}
+        />
+    );
+};
---
+++ src/services/customLib.js
@@ -0,0 +1,62 @@
+export const customLib = () => {
+
+};
+
+customLib.common = 'the, it is, we all, a, an, by, to, you, me, he, she, they, we, how, it, i, are, to, for, of, .,';
+
+customLib.filterArray = (array, filterOptions) => {
+    if (filterOptions.name.length > 0) {
+        array = array.filter((movie) => {
+            return movie.name.trim().toLowerCase().indexOf(filterOptions.name.trim().toLowerCase()) !== -1;
+        });
+    }
+    if (filterOptions.genreIds.length > 0) {
+        array = array.filter((movie) => {
+            return filterOptions.genreIds.map((genreId) => {
+                return genreId.id;
+            }).every(elem => movie.genreIds.includes(elem));
+        });
+    }
+    if (filterOptions.adult) {
+        array = array.filter((movie) => {
+            return movie.adult;
+        });
+    }
+    if (filterOptions.desc) {
+        array = array.filter((movie) => {
+            return customLib.getUncommon(filterOptions.desc, customLib.common)
+                .some(elem => customLib.getUncommon(movie.desc, customLib.common).includes(elem));
+        });
+    }
+    return array;
+};
+
+customLib.getUncommon = (sentence, common) => {
+    var wordArr = sentence.match(/\w+/g),
+        commonObj = {},
+        uncommonArr = [],
+        word, i;
+
+    common = common.split(',');
+    for ( i = 0; i < common.length; i++ ) {
+        commonObj[ common[i].trim() ] = true;
+    }
+
+    for ( i = 0; i < wordArr.length; i++ ) {
+        word = wordArr[i].trim().toLowerCase();
+        if ( !commonObj[word] ) {
+            uncommonArr.push(word);
+        }
+    }
+
+    return uncommonArr;
+};
+
+customLib.arrayIsNotEmpty = (array) => {
+    return typeof array !== 'undefined' && array !== null && array.length > 0;
+};
+
+customLib.arrayIsEmpty = (array) => {
+    return typeof array === 'undefined' || array === null || array.length === 0;
+};
+
